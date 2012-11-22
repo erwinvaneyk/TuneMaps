08681f9be5cfd5b70152f2d3477794fc2b7b8fe6
Erwin van Eyk <Erwin_dso@hotmail.com>
2012-11-22 11:05:09 +0100

    Added external javaScript-file for the musicplayer

--- src/TuneMaps/FrontBundle/Resources/logic/crawler/test.php
+++ src/TuneMaps/FrontBundle/Resources/logic/crawler/test.php
@@ -28,54 +28,6 @@ if(!$youtubeURI) {
 	<script src="http://www.youtube.com/iframe_api"></script>
 	<script src="http://code.jquery.com/jquery-1.8.3.min.js"></script>
 	<script>
-	  function Player(id,element) { 
-	  
-		var iPlayer = new YT.Player(element, {
-		  height: '200',
-		  width: '200',
-		  videoId: id,
-		  playerVars: { 'autoplay': 1, 'controls':0, 'showinfo':0 },
-		  events: {
-			//'onReady': onPlayerReady,
-		  }
-		});
-		
-		iPlayer.stopTrack = function() {
-			this.stopVideo();
-		}
-		
-		iPlayer.pauseTrack = function() {
-			this.pauseVideo();
-		}
-		
-		iPlayer.playTrack = function() {
-			this.playVideo();
-		}
-		
-		iPlayer.getUrl = function() {
-			return this.getVideoUrl();
-		}
-		
-		return iPlayer;
-	  }
-	  
-	  function onYouTubeIframeAPIReady() {
-		player = new Player('<?php echo $youtubeURI; ?>','player');
-	  }
-	
-	$(document).ready(function(){
-		$("#footer").hide();
-		var bigFooter = false;
-		$("#topfooter").click(function(event) {
-			if(bigFooter) {
-				$("#footer").slideUp();
-				bigFooter = false;
-			} else {
-				$("#footer").slideDown();
-				bigFooter = true;
-			}			
-		});
-	});
     </script>
 </head>
 
