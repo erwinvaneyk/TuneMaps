362073bb3bd00b74a3037dd0020feccc2543ac72
Erwin van Eyk <Erwin_dso@hotmail.com>
2012-12-03 15:32:07 +0100

    Added musicSource entity to ServiceBundle

--- src/TuneMaps/RecommendationBundle/Entity/Event.php
+++ src/TuneMaps/RecommendationBundle/Entity/Event.php
@@ -62,6 +62,15 @@ class Event implements \JsonSerializable
 		$this->name = $name;
 	}
 	
+        public function setId($id) {
+            $this->id = $id;
+        }
+        
+        public function getId() {
+            return $this->id;
+        }
+        
+        
 	public function getDateTime() {
 		return $this->datetime;
 	}
