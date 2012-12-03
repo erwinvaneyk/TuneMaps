f5f63fb6d7519504820572bf4736d8d3336a2c51
Rolf Jagerman <rjagerman@gmail.com>
2012-12-03 16:37:24 +0100

    Removed duplicate getters and setters

--- src/TuneMaps/RecommendationBundle/Entity/Event.php
+++ src/TuneMaps/RecommendationBundle/Entity/Event.php
@@ -69,14 +69,6 @@ class Event implements \JsonSerializable
 	public function setName($name) {
 		$this->name = $name;
 	}
-	
-        public function setId($id) {
-            $this->id = $id;
-        }
-        
-        public function getId() {
-            return $this->id;
-        }
         
         
 	public function getDateTime() {
