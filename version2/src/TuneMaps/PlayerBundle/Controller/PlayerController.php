8e90b0c98db906dd22d4ddd5a46f82580cf265d2
Erwin van Eyk <Erwin_dso@hotmail.com>
2013-01-09 15:10:06 +0100

    updated playercontroller to retrieve results from db

--- version2/src/TuneMaps/PlayerBundle/Controller/PlayerController.php
+++ version2/src/TuneMaps/PlayerBundle/Controller/PlayerController.php
@@ -8,6 +8,9 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 use TuneMaps\CrawlerBundle\Entity\YoutubeCrawler;
+use TuneMaps\MusicDataBundle\Entity\Song;
+use TuneMaps\MusicDataBundle\Entity\Artist;
+use TuneMaps\MusicDataBundle\Entity\ArtistPlayed;
 
 class PlayerController extends Controller
 {
@@ -15,12 +18,28 @@ class PlayerController extends Controller
     /**
      * Searches for a track and gives the youtube uri
      * 
-     * @Route("/player/{artist}/{title}", name="youtubecode")
+     * @Route("/player/{artist_name}/{tracktitle}", name="youtubecode")
      */
-    function youtubeCodeAction($artist, $title) {
-        $youtubeCrawler = new YoutubeCrawler();
-        $youtube = $youtubeCrawler->getFirstVideo($artist . ' ' . $title);
-        $json = array('artist' => $artist, 'title' => $title, 'youtube' => $youtube);
+    function youtubeCodeAction($artist_name, $tracktitle) {
+        $json = null;
+        //try to find code internally
+        $em = $this->getDoctrine()->getEntityManager();
+        if(($artist = $em->getRepository('TuneMaps\MusicDataBundle\Entity\Artist')->findOneBy(array('name' => $artist_name))) != null) {
+            if(($song = $em->getRepository('TuneMaps\MusicDataBundle\Entity\Song')->findOneBy(array('title' => $tracktitle, 'artist' => $artist))) != null) {
+                $json = array('artist' => $artist_name, 'title' => $title, 'youtube' => $song->{'youtube'});
+            }
+        }
+        
+        //retrieve code externally
+        if($json == null) {
+            //retrieve  code from youtube
+            $youtubeCrawler = new YoutubeCrawler();
+            $youtube = $youtubeCrawler->getFirstVideo($artist_name . ' ' . $tracktitle);
+            $json = array('artist' => $artist_name, 'title' => $title, 'youtube' => $youtube);
+            //save song/artist
+        }
+        
+        //return result
         return new JsonResponse($json);
     }
     
