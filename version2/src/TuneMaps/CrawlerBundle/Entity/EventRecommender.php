0756afcc7b760b5e92d834e5cce9a03046a37523
Dennis Eikelenboom <denniseik@gmail.com>
2013-01-10 10:32:31 +0100

    EventRecommender aangemaakt

---
+++ version2/src/TuneMaps/CrawlerBundle/Entity/EventRecommender.php
@@ -0,0 +1,26 @@
+<?php
+/*
+ * @author D. Eikelenboom
+ */
+namespace TuneMaps\CrawlerBundle\Entity;
+
+use TuneMaps\MusicDataBundle\Entity\Artist;
+use TuneMaps\MusicDataBundle\Entity\Event;
+use TuneMaps\MusicDataBundle\Entity\Location;
+use TuneMaps\MusicDataBundle\Entity\Song;
+use TuneMaps\MusicDataBundle\Entity\Venue;
+
+/**
+ * A crawler for Last FM
+ */
+class EventRecommender {
+    
+    /**
+     * get
+     */
+    protected function getEvents() {
+        return null;
+    }
+    
+}
+?>
